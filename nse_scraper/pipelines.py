39080a1078ec40e2cd473ba646d7cc3c548e9c16
folukeakpobasa <folukeakpobasa@decagon.dev>
2024-04-24 13:55:33 +0100

    feat: create pipeline

--- nse_scraper/pipelines.py
+++ nse_scraper/pipelines.py
@@ -6,8 +6,57 @@
 
 # useful for handling different item types with a single interface
 from itemadapter import ItemAdapter
+import pymongo
+from scrapy.exceptions import DropItem
+from .items import NseScraperItem
 
 
 class NseScraperPipeline:
-    def process_item(self, item, spider):
-        return item
+   collection = "stock_data"
+
+   def __init__(self, mongodb_uri, mongo_db):
+       self.db = None
+       self.client = None
+       self.mongodb_uri = mongodb_uri
+       self.mongo_db = mongo_db
+       if not self.mongodb_uri:
+           raise ValueError("MongoDB URI not set")
+       if not self.mongo_db:
+           raise ValueError("Mongo DB not set")
+
+   @classmethod
+   def from_crawler(cls, crawler):
+       return cls(
+           mongodb_uri=crawler.settings.get("MONGODB_URI"),
+           mongo_db=crawler.settings.get('MONGO_DATABASE', 'nse_data')
+       )
+
+   def open_spider(self, spider):
+       self.client = pymongo.MongoClient(self.mongodb_uri)
+       self.db = self.client[self.mongo_db]
+
+   def close_spider(self, spider):
+       self.client.close()
+  
+   def clean_stock_data(self,item):
+       if item['ticker_symbol'] is None:
+           raise DropItem('Missing ticker symbol in %s' % item)
+       elif item['stock_name'] == 'None':
+           raise DropItem('Missing stock name in %s' % item)
+       elif item['stock_price'] == 'None':
+           raise DropItem('Missing stock price in %s' % item)
+       else:
+           return item
+
+   def process_item(self, item, spider):
+       """
+       process item and store to database
+       """
+     
+       clean_stock_data = self.clean_stock_data(item)
+       data = dict(NseScraperItem(clean_stock_data))
+       print(data)
+       # print(self.db[self.collection].insert_one(data).inserted_id)
+       self.db[self.collection].insert_one(data)
+
+       return item
